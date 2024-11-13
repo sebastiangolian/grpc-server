df1e385d808de52c5144b32486c6d3d51a07e894
Sebastian Golian <sebastiangolian@solwit.com>
2024-11-13 10:50:39 +0100

    update

--- server.py
+++ server.py
@@ -3,16 +3,25 @@ import grpc
 import greet_pb2
 import greet_pb2_grpc
 from grpc_reflection.v1alpha import reflection
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class GreeterService(greet_pb2_grpc.GreeterServicer):
     def SayHello(self, request, context):
+        logger.info(f"Received request: {request.name}")
+        
+        metadata = dict(context.invocation_metadata())
+        if metadata:
+            logger.info(f"Metadata received: {metadata}")
+        
         return greet_pb2.HelloReply(message=f"Hello, {request.name}!")
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     greet_pb2_grpc.add_GreeterServicer_to_server(GreeterService(), server)
     
-    # Enable reflection
     SERVICE_NAMES = (
         greet_pb2.DESCRIPTOR.services_by_name['Greeter'].full_name,
         reflection.SERVICE_NAME,
@@ -21,7 +30,7 @@ def serve():
 
     server.add_insecure_port('[::]:50051')
     server.start()
-    print("Server is running on port 50051...")
+    logger.info("Server is running on port 50051...")
     server.wait_for_termination()
 
 if __name__ == '__main__':
